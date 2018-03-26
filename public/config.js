2e345dae01f4bdb0306c18522a0540337ea2c6b2
raycen001 <cabbage.cen@hotmail.com>
2018-03-26 08:35:49 +0800

    UPDATE CONFIG

--- public/config.js
+++ public/config.js
@@ -4,14 +4,14 @@ return {
     "configurationArguments": {
         "save": {
             // "verb": "POST", 
-            "url": "https://jen-sfmc.herokuapp.com/sfmcApp/activities/bridge-app/save/", 
+            "url": "https://damp-ocean-68990.herokuapp.com/sfmcApp/activities/bridge-app/save/", 
             // "headers": "", 
             "body": "", 
             "useJwt": false
         }, 
         "validate": {
             // "verb": "POST", 
-            "url": "https://jen-sfmc.herokuapp.com/sfmcApp/activities/bridge-app/validate/", 
+            "url": "https://damp-ocean-68990.herokuapp.com/sfmcApp/activities/bridge-app/validate/", 
             // "headers": "", 
             "body": "", 
             "useJwt": false
@@ -20,7 +20,7 @@ return {
         "applicationExtensionKey": "quJw2wUP0wkUOSACyVhk9HfTBweWAjcIgyvNY-fGCBbW1EFCqEOKZDPlMUIOuaaXae3w-vnEoQf7Vv0Ssz2UR-2eoNhjV5N3NaehzhRsMV-6AQkvZdKHozcWpe87tkX76qBOkq3XDMBqyZ1U4FLJ51hynfBVP38IskUeDvEMMCGDuaURqR7L8_utDbPh6xnF5jk4TTzdE7cG3UBN05zAYfUXwZKSH5Zl8BAt1AvwIcqJT5HFbn837DSbSBcA5g2", 
         "publish": {
             // "verb": "POST", 
-            "url": "https://jen-sfmc.herokuapp.com/sfmcApp/activities/bridge-app/publish/", 
+            "url": "https://damp-ocean-68990.herokuapp.com/sfmcApp/activities/bridge-app/publish/", 
             // "headers": "", 
             "body": "", 
             "useJwt": false
@@ -28,13 +28,13 @@ return {
     }, 
     "metaData": {
         "version": "2.0", 
-        "iconSmall": "https://jen-sfmc.herokuapp.com/images/sms1.png", 
-        "icon": "https://jen-sfmc.herokuapp.com/images/sms.png"
+        "iconSmall": "https://damp-ocean-68990.herokuapp.com/images/sms1.png", 
+        "icon": "https://damp-ocean-68990.herokuapp.com/images/sms.png"
     }, 
     "type": "REST", 
     "edit": {
         "width": 500, 
-        "url": "https://jen-sfmc.herokuapp.com/sfmcApp/activities/bridge-app/", 
+        "url": "https://damp-ocean-68990.herokuapp.com/sfmcApp/activities/bridge-app/", 
         "height": 400
     }, 
     "lang": {
@@ -54,7 +54,7 @@ return {
                 }
             ], 
             "header": "", 
-            "url": "https://jen-sfmc.herokuapp.com/sfmcApp/activities/bridge-app/execute/", 
+            "url": "https://damp-ocean-68990.herokuapp.com/sfmcApp/activities/bridge-app/execute/", 
             "format": "json", 
             "useJwt": false, 
             "outArguments": []
